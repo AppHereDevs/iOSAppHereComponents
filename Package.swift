d841e60b7e01ceb672927674bfcc7bc9a7a99279
Arda Onat <ardonat97@hotmail.com>
2021-12-24 00:25:33 +0300

    Change AppHereCore package version.

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["AppHereComponents"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/AppHereDevs/AppHereCore.git", .exact("1.0.2"))
+        .package(url: "https://github.com/AppHereDevs/AppHereCore.git", .exact("1.0.3"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
