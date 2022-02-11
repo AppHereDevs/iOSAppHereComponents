36b63047654fd7a6ab8a5e79841a76b157e34f8e
Arda Onat <ardonat97@hotmail.com>
2022-02-11 16:07:05 +0300

    Fix AppHereButtonTests.

--- Tests/AppHereComponentsTests/AppHereButtonTests.swift
+++ Tests/AppHereComponentsTests/AppHereButtonTests.swift
@@ -45,7 +45,7 @@ final class AppHereButtonTest: XCTestCase {
         sut.themeKey = "AppHereButton-NoValue"
 
         // Then
-        XCTAssertEqual(sut.titleLabel?.textColor, UIColor(hexString: AppHereButtonThemeModel.defaultTextColor))
+        XCTAssertEqual(sut.tintColor, UIColor(hexString: AppHereButtonThemeModel.defaultTextColor))
         XCTAssertEqual(sut.backgroundColor, UIColor(hexString: AppHereButtonThemeModel.defaultBackgroundColor))
         XCTAssertEqual(sut.layer.cornerRadius, 0)
         XCTAssertEqual(sut.layer.borderWidth, 0)
@@ -60,7 +60,7 @@ final class AppHereButtonTest: XCTestCase {
         sut.themeKey = "AppHereButton-Values"
 
         // Then
-        XCTAssertEqual(sut.titleLabel?.textColor, UIColor(hexString:"#FFF000"))
+        XCTAssertEqual(sut.tintColor, UIColor(hexString:"#FFF000"))
         XCTAssertEqual(sut.backgroundColor, UIColor(hexString:"#0CBFDC"))
         XCTAssertEqual(sut.layer.cornerRadius, 8)
         XCTAssertEqual(sut.layer.borderWidth, 8)
