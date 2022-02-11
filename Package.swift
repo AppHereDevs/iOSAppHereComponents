b03f13a38f10f359be58055e9405f1d18572dfee
Muhammed Sevuk <sevukmuhammed@gmail.com>
2022-02-12 01:31:37 +0300

    AppHereInputView validation added

--- Package.swift
+++ Package.swift
@@ -15,14 +15,15 @@ let package = Package(
             targets: ["AppHereComponents"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ArdOnat/CoreModule.git", .exact("1.1.6"))
+        .package(url: "https://github.com/ArdOnat/CoreModule.git", .exact("1.1.6")),
+        .package(name: "Validator", url: "https://github.com/adamwaite/Validator.git", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AppHereComponents",
-            dependencies: ["CoreModule"]),
+            dependencies: ["CoreModule", "Validator"]),
         .testTarget(
             name: "AppHereComponentsTests",
             dependencies: ["AppHereComponents"],
--- Sources/AppHereComponents/Components/AppHereInputView/AppHereInputViewModel.swift
+++ Sources/AppHereComponents/Components/AppHereInputView/AppHereInputViewModel.swift
@@ -10,16 +10,48 @@ import UIKit
 public struct AppHereInputViewModel {
     var title: String?
     var phoneLabelText: String?
-    var placeholder: String?
+    var errorLabelText: String?
+    var placeholder: String
     var regex: String?
     var keyboardType: UIKeyboardType
+    let validationModel: ValidationModel?
     
-    public init(title: String?, phoneLabelText: String?, placeholder: String?, regex: String?, keyboardType: UIKeyboardType = .default) {
+    public init(title: String? = nil,
+                phoneLabelText: String? = nil,
+                errorLabelText: String? = nil,
+                placeholder: String,
+                regex: String? = nil,
+                keyboardType: UIKeyboardType = .default,
+                validationModel: ValidationModel? = nil) {
         
         self.title = title
         self.phoneLabelText = phoneLabelText
         self.placeholder = placeholder
         self.regex = regex
         self.keyboardType = keyboardType
+        self.errorLabelText = errorLabelText
+        self.validationModel = validationModel
+    }
+    
+    public struct ValidationModel {
+        let patternType: PatternType?
+        let regexPattern: String?
+        let maxCharLength: Int?
+        //TODO: IsSecureTextEntry will add
+        
+        public init(patternType: PatternType? = nil,
+                    regexPattern: String? = nil,
+                    maxCharLength: Int? = nil) {
+            
+            self.patternType = patternType
+            
+            if let pattern = patternType,
+               let regex = pattern.regex {
+                self.regexPattern = regex
+            } else {
+                self.regexPattern = regexPattern
+            }
+            self.maxCharLength = maxCharLength
+        }
     }
 }
