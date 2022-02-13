d926f2357fcd21aef9ff24632df727348610ba35
Arda Onat <ardonat97@hotmail.com>
2022-02-13 18:08:17 +0300

    Implement start timer function for AppHereLabel.

--- Sources/AppHereComponents/UI Elements/AppHereLabel/AppHereLabel.swift
+++ Sources/AppHereComponents/UI Elements/AppHereLabel/AppHereLabel.swift
@@ -11,6 +11,8 @@ public class AppHereLabel: UILabel, Themeable {
     
     public var themeDict: NSDictionary?
     
+    private var seconds: Int = 0
+    
     public var themeKey: String? {
         didSet {
             guard let themeKey = themeKey, let themeDict = AppHereThemeManager.shared.getTheme(byKey: themeKey) else {
@@ -38,5 +40,23 @@ public class AppHereLabel: UILabel, Themeable {
             layer.masksToBounds = true
         }
         
+    }
+    
+    public func startTimer(seconds: Int) {
+        self.seconds = seconds
+        var timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimerText), userInfo: nil, repeats: true)
+    }
+    
+    @objc func update() {
+        seconds -= 1
+        self.text = seconds.timerString
+    }
+}
+
+extension Int {
+    var timerString: String {
+        let minutes = self / 60 % 60
+        let seconds = self % 60
+        return String(format:"%02i:%02i:%02i", hours, minutes, seconds)
     }
 }
