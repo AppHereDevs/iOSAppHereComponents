792c1e92e39e45ad7fe7632c3a79e754925b464e
Arda Onat <arda.onat@global.com>
2022-06-11 23:11:44 +0100

    Nice

--- Sources/AppHereComponents/Components/AppHereAlertView/AppHereAlertView.swift
+++ Sources/AppHereComponents/Components/AppHereAlertView/AppHereAlertView.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AppHereAlertView: AppHereComponentView {
+public class AppHereAlertView: AppHereComponentView {
     
     @IBOutlet private weak var alertIconImageView: UIImageView!
     @IBOutlet private weak var alertLabel: AppHereLabel!
