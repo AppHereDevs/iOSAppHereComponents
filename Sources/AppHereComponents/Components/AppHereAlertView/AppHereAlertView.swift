6e327098f96bc726bd9dcf042e8ddc974632a61a
Arda Onat <arda.onat@global.com>
2022-06-11 23:09:06 +0100

    Nice

---
+++ Sources/AppHereComponents/Components/AppHereAlertView/AppHereAlertView.swift
@@ -0,0 +1,37 @@
+//
+//  AppHereAlertView.swift
+//  ooAutos
+//
+//  Created by Arda Onat on 11/06/2022.
+//
+
+import UIKit
+
+class AppHereAlertView: AppHereComponentView {
+    
+    @IBOutlet private weak var alertIconImageView: UIImageView!
+    @IBOutlet private weak var alertLabel: AppHereLabel!
+    @IBOutlet private weak var cancelButton: AppHereButton!
+    @IBOutlet private weak var approveButton: AppHereButton!
+    
+    public var cancelButtonTapHandler: (() -> Void)?
+    public var approveButtonTapHandler: (() -> Void)?
+    
+    public init(imageName: String, alertText: String) {
+        super.init(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        self.alertIconImageView.image = UIImage(named: imageName)
+        self.alertLabel.text = alertText
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    @IBAction private func cancelButtonPressed(_ sender: Any) {
+        cancelButtonTapHandler?()
+    }
+    
+    @IBAction private func approveButtonTapHandler(_ sender: Any) {
+        approveButtonTapHandler?()
+    }
+}
