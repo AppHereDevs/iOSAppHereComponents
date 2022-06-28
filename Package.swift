8e75a16288dcd0bfe16126ad2d4fd0f0204bcac5
Arda Onat <arda.onat@global.com>
2022-06-28 18:46:06 +0100

    Update packages.

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/ArdOnat/CoreModule.git", .exact("1.3.2")),
-        .package(name: "Validator", url: "https://github.com/adamwaite/Validator.git", branch: "master"),
-        .package(name: "Kingfisher", url: "https://github.com/onevcat/Kingfisher.git", .exact("6.0.0"))
+        .package(name: "Validator", url: "https://github.com/adamwaite/Validator.git", branch: "3.2.1"),
+        .package(name: "Kingfisher", url: "https://github.com/onevcat/Kingfisher.git", .exact("7.2.4"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
